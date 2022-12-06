6b596a315e27bd3e41833e3b0ada24ca90a11da6
Andrey Drenski <andreshko.dre@gmail.com>
2022-12-06 08:34:11 +0200

    add day06 solutions

---
+++ Day6.cpp
@@ -0,0 +1,41 @@
+#include <fmt/core.h>
+#include <fstream>
+#include <string>
+#include <algorithm> // std::ranges::{sort,adjacent_find}
+#include <ranges> // std::views::slide
+
+int day6(const char* filename, const int n) {
+#if 1
+	// Allocates only 2*N bytes, reads file byte by byte
+	std::string buff(n, '\0'); // SSO to the rescue
+	std::ifstream file{ filename };
+	file.read(buff.data(), n);
+	for (int i = 0; file; file.get(buff[i % n]), ++i) {
+		std::string tmp = buff;
+		std::ranges::sort(tmp);
+		if (std::ranges::adjacent_find(tmp) == tmp.end()) {
+			return i + n;
+		}
+	}
+#else
+	// Heap memory goes brrr
+	std::string line;
+	std::ifstream{ filename } >> line; // no whitespace in input
+	for (std::span<char> rng : line | std::views::slide(n)) {
+		std::string tmp(rng.begin(), rng.end());
+		std::ranges::sort(tmp);
+		if (std::ranges::adjacent_find(tmp) == tmp.end()) {
+			return rng.data() - line.data() + n;
+		}
+	}
+	return 0;
+#endif
+}
+
+int main6() {
+	fmt::print("{}\n", day6("input/6test.txt", 4));  // 7
+	fmt::print("{}\n", day6("input/6full.txt", 4));  // 1134
+	fmt::print("{}\n", day6("input/6test.txt", 14)); // 19
+	fmt::print("{}\n", day6("input/6full.txt", 14)); // 2263
+	return 0;
+}
