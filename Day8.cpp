9fc4bf4a46e8180264107d9830c98d0e5050d401
Andrey Drenski <andreshko.dre@gmail.com>
2022-12-08 15:38:13 +0200

    implement & use a range of int pairs - may be useful in later challenges

--- Day8.cpp
+++ Day8.cpp
@@ -1,20 +1,9 @@
 #include "Lines.h"
+#include "IntPairs.h"
 #include <fmt/core.h>
 #include <vector>
-#include <ranges> // std::views::{iota,reverse}
-#include <algorithm> // std::ranges::{find_if,count,all_of,max}
-
-// Generates a range of f(i,j) for each i in [0;n) and each j in [0;m).
-// Same as transform(cartesian_product(iota(0,n),iota(0,m)),
-//                   [&](const auto [i,j]) { ... })) - if it were supported (:
-template <typename Func>
-auto genPairs(const int n, const int m, Func&& f) -> cppcoro::generator<std::decay_t<std::invoke_result_t<Func, int, int>>> {
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < m; ++j) {
-			co_yield f(i, j);
-		}
-	}
-}
+#include <ranges> // std::views::{iota,reverse,transform}
+#include <algorithm> // std::ranges::{find_if,count_if,all_of,max}
 
 // Counts the number of values in r before the first that
 // satisfies the predicate, but includes it in the count.
@@ -31,20 +20,22 @@ void day8(const char* filename) {
 	using std::views::reverse;
 	const int n = int(map.size());
 	const int m = int(map[0].size());
-	const auto part1 = std::ranges::count(genPairs(n, m, [&](const int i, const int j) {
+	const auto part1 = std::ranges::count_if(IntPairs{ n,m }, [&](const IntPair p) { // Would have been cool to have structed bindings here (:
+		const auto& [i, j] = p;
 		const int curr = map[i][j];
 		return std::ranges::all_of(iota(0, i), [&](int k) { return map[k][j] < curr; })
 			|| std::ranges::all_of(iota(i + 1, n), [&](int k) { return map[k][j] < curr; })
 			|| std::ranges::all_of(iota(0, j), [&](int k) { return map[i][k] < curr; })
 			|| std::ranges::all_of(iota(j + 1, m), [&](int k) { return map[i][k] < curr; });
-		}), true);
-	const auto part2 = std::ranges::max(genPairs(n, m, [&](const int i, const int j) {
+		});
+	const auto part2 = std::ranges::max(IntPairs{ n,m } | std::views::transform([&](const IntPair p) {
+		const auto& [i, j] = p;
 		const char curr = map[i][j];
 		return countUntil(reverse(iota(0, i)), [&](int k) { return map[k][j] >= curr; })
 			 * countUntil(iota(i + 1, n), [&](int k) { return map[k][j] >= curr; })
 			 * countUntil(reverse(iota(0, j)), [&](int k) { return map[i][k] >= curr; })
 			 * countUntil(iota(j + 1, n), [&](int k) { return map[i][k] >= curr; });
-	}));
+		}));
 	fmt::print("{} {}\n", part1, part2);
 }
 
